8eb4e726c2ec0eee556b105949660ba42afce3c7
TELANGANA CHESS ACADEMY <telanganachessacademy@gmail.com>
2026-01-10 18:18:57 +0530

    Update EVENTS link to the correct URL

--- components/header.tsx
+++ components/header.tsx
@@ -18,7 +18,7 @@ export function Header() {
   const navItems: NavItem[] = [
     { name: "Home", href: "/" },
     { name: "ONLINE COACHING", href: "https://svc-ui-7.netlify.app/login" },
-    { name: "EVENTS", href: "https://telanganachessacademy.com/tournaments/" },
+    { name: "EVENTS", href: "https://www.telanganachessacademy.com/events" },
     { name: "Courses", href: "/courses" },
     { name: "Our Coaches", href: "/coaches" },
     { name: "Gallery", href: "/gallery" },
